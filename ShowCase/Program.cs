e88c08bb01f86a313042e95cdf0874c09eac130c
Ayowumi Adeyeye <aadeyey1@my.centennialcollege.ca>
2025-04-05 00:29:29 -0400

    Initial commit

---
+++ ShowCase/Program.cs
@@ -0,0 +1,17 @@
+﻿using StringLibrary;
+
+namespace ShowCase;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string testString1 = "Hello, Azure DevOps!";
+        string testString2 = "hello, azure devops!";
+
+        Console.WriteLine($"'{testString1}' starts with uppercase: {testString1.StartsWithUpper()}");
+        Console.WriteLine($"'{testString2}' starts with uppercase: {testString2.StartsWithUpper()}");
+
+        Console.ReadLine();
+    }
+}
