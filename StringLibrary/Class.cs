c932793f4ce81d012bcddf0777f30591a932837c
Ayowumi Adeyeye <aadeyey1@my.centennialcollege.ca>
2025-04-05 00:35:08 -0400

    Refactor StartsWithUpperCase

--- StringLibrary/Class.cs
+++ StringLibrary/Class.cs
@@ -2,13 +2,9 @@
 {
     public static class StringLibrary
     {
-        public static bool StartsWithUpper(this string str)
+        public static bool StartsWithUpperCase(this string str)
         {
-            if (string.IsNullOrWhiteSpace(str))
-                return false;
-
-            char ch = str[0];
-            return char.IsUpper(ch);
+            return !string.IsNullOrWhiteSpace(str) && char.IsUpper(str[0]);
         }
     }
 }
